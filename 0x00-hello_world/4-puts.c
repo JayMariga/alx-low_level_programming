07c30d3c6323b63317e79a00ded59af0ff149f03
joseph <marigajoseph8046@gmail.com>
2023-10-05 07:13:40 -0700

    tried learning my first c command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ 
+	/**
+	 * string initialisation
+	 *char mystr[] = "Programming is like building a multilingual puzzle";
+	 *
+	 * *puts(mystr); 
+	 *
+	 * return 0;
+}
