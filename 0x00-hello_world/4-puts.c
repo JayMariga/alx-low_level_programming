a7fa4127b6241d3dc35f3d0bad82dd43d6aa1db5
joseph <marigajoseph8046@gmail.com>
2023-10-05 07:16:54 -0700

    simplified format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
-#include<stdio.h>
-int main()
-{ 
-	/**
-	 * string initialisation
-	 *char mystr[] = "Programming is like building a multilingual puzzle";
-	 *
-	 * *puts(mystr); 
-	 *
-	 * return 0;
+#include <stdio.h>
+
+int main() 
+{
+	    puts("Programming is like building a multilingual puzzle");
+	        return 0;
 }
+
