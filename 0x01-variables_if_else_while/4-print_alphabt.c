6fe42c4f8030c74833dac3f1241ffd0df98907ba
joseph <marigajoseph8046@gmail.com>
2023-10-06 19:31:29 -0700

    lowercase only

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -should be main block
+ * Description: pick a number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
