79b96ee9d1b3ad6c2ed426342ff45e8f1dc05da5
joseph <marigajoseph8046@gmail.com>
2023-10-11 09:00:34 -0700

    alphabetic

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet -should be Entry point
+* Return:is 0
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+
+		c++;
+
+	}
+	_putchar('\n');
+
+}
