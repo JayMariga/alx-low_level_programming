ecd447704174a5d35af93f379c82d8ab551610e0
joseph <marigajoseph8046@gmail.com>
2023-10-12 01:36:38 -0700

    use of mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - gives the product of its parameters
+ * @a: the integer type number
+ * @b: the integer type number
+ * Return:0
+ */
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
