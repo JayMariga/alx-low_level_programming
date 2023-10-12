2303d4e60a7b57f81e7ceb63a1f02a69e70c2f9e
joseph <marigajoseph8046@gmail.com>
2023-10-12 02:27:51 -0700

    is digit forgotten

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - should check for a digit
+ * @c : the character to be checked
+ *
+ * Return:0 or 1
+ */
+
+int _isdigit(int c)
+{
+return (c >= 48 && c <= 57);
+
+}
