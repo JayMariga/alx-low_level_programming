a756dd23f383fd0ea2cab273a71aec69d71b94ea
joseph <marigajoseph8046@gmail.com>
2023-10-19 03:21:51 -0700

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,18 +4,18 @@
  * reverse_array - should reverse an int array
  * @a: should be pointer
  * @n: is the array length
- * 
- * Return: void
+ *
+ * Return: (void)
 */
 
 void reverse_array(int *a, int n)
 {
-	int i = 0, half;
+int i = 0, half;
 
-	for (half = n / 2; half > 0; half--, i++)
-	{
-		a[n - i - 1] += a[i];
-		a[i] = a[n - i - 1] - a[i];
-		a[n - i - 1] = a[n - i - 1] - a[i];
-	}
+for (half = n / 2; half > 0; half--, i++)
+{
+a[n - i - 1] += a[i];
+a[i] = a[n - i - 1] - a[i];
+a[n - i - 1] = a[n - i - 1] - a[i];
+}
 }
