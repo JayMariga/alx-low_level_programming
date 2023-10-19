79fbf2f01f9d84585ed777be6ef909a02fe670c0
joseph <marigajoseph8046@gmail.com>
2023-10-19 03:19:22 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - should reverse an int array
+ * @a: should be pointer
+ * @n: is the array length
+ * 
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, half;
+
+	for (half = n / 2; half > 0; half--, i++)
+	{
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
+	}
+}
