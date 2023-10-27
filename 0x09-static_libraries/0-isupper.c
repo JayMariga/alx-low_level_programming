dd8006bee5aed2b0cc42d49c06c03fc7da2aa9f8
joseph <marigajoseph8046@gmail.com>
2023-10-27 14:45:47 -0700

    not easy

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Is for checking the code for
+ * Holberton School students.
+ * @c: An integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isupper(int c)
+{
+
+return (c >= 65 && c <= 90);
+}
