b392d37a64d0d00faf64317e934c7e6ccaf89a3e
joseph <marigajoseph8046@gmail.com>
2023-10-27 13:50:58 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Will be the function
+ * @argc: The offset count
+ * @argv: And the offset value
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
